1fd76e9474b08556502454e5a99ec4a8b7acbbf0
Oleh Yaroshenko <oleg21062010@gmail.com>
2025-08-14 19:06:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 const elementsArray = [];
 
-fetch('http://localhost:000')
+fetch('http://localhost:3000/')
   .then(response => response.json())
   .then(data => {
     elementsArray.push(...data);
-    const container = document.createElement('div');
+    const container = document.querySelector(".wrapper")
     elementsArray.forEach(item => {
-      container.innerHTML += <div>${item.html}</div>;
+      container.innerHTML += `<div>
+      <h3>${item.name}</h3>
+      <p>${item.description}</p>
+      <p>${item.price}</p>
+      </div>`;
     });
     document.body.appendChild(container);
   })
   .catch(error => {
     console.error('Fetch error:', error);
   });
+
